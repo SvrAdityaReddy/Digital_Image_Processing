d950367ea5ebff775a17af955369fc25ed48024b
SvrAdityaReddy <aditya.seelapureddy@yahoo.com>
2017-01-25 19:19:30 +0530

    nature_image_sobel_filter

--- Code/gradient.py
+++ Code/gradient.py
@@ -2,12 +2,12 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread("/home/aditya/DIP/DIP17_1_Images/pipe.jpg")
+img = cv2.imread("/home/aditya/DIP/DIP17_1_Images/pipe.jpeg")
 img_grey=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-cv2.imwrite("/home/aditya/DIP/DIP17_1_Images/pipe_GREY.jpg",img_grey)
+cv2.imwrite("/home/aditya/DIP/DIP17_1_Images/pipe_GREY.jpeg",img_grey)
 sobelx = cv2.Sobel(img,cv2.CV_8U,1,0,ksize=3)
 sobely = cv2.Sobel(img,cv2.CV_8U,0,1,ksize=3)
 # s_sobel64f = np.absolute(sobelx64f)
 # sobel_8u = np.uint8(abs_sobel64f)
-cv2.imwrite("/home/aditya/DIP/DIP17_1_Images/pipe_y_edge_GREY.jpg",sobelx)
-cv2.imwrite("/home/aditya/DIP/DIP17_1_Images/pipe_x_edge_GREY.jpg",sobely)
+cv2.imwrite("/home/aditya/DIP/DIP17_1_Images/pipe_y_edge_GREY.jpeg",sobelx)
+cv2.imwrite("/home/aditya/DIP/DIP17_1_Images/pipe_x_edge_GREY.jpeg",sobely)
