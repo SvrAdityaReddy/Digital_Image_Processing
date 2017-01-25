6c89452a0aeda028f61e1ddc16fe9cc3ac33a535
SvrAdityaReddy <aditya.seelapureddy@yahoo.com>
2017-01-25 22:05:23 +0530

    edge_mapping_thresholding_blurring

---
+++ Code/edge_mapping.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import math
+
+img = cv2.imread("/home/aditya/DIP/DIP17_1_Images/pipe.jpeg")
+img_grey=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.imwrite("/home/aditya/DIP/DIP17_1_Images/pipe_GREY.jpeg",img_grey)
+g_blur = cv2.GaussianBlur(img_grey,(5,5),math.sqrt(2))
+cv2.imwrite("/home/aditya/DIP/DIP17_1_Images/pipe_blur_GREY.jpeg",g_blur)
+sobelx = cv2.Sobel(g_blur,cv2.CV_8U,1,0,ksize=3)
+sobely = cv2.Sobel(g_blur,cv2.CV_8U,0,1,ksize=3)
+gradient_image = sobelx + sobely
+cv2.imwrite("/home/aditya/DIP/DIP17_1_Images/pipe_blur_gradient_GREY.jpeg",gradient_image)
+(height,width) = gradient_image.shape
+threshold = gradient_image[0][0]
+for h in range(height):
+    for w in range(width):
+        if threshold < gradient_image[h][w]:
+            threshold = gradient_image[h][w]
+threshold = threshold * (33.00/100)
+
+for h in range(height):
+    for w in range(width):
+        if gradient_image[h][w] < threshold:
+            gradient_image[h][w] = 0
+
+cv2.imwrite("/home/aditya/DIP/DIP17_1_Images/pipe_blur_gradient_GREY_threshold_33%.jpeg",gradient_image)
+
+
+
+
